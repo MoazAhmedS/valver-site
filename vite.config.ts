82a9f36c256d0ed9acc5b72dcb72e10059da8bcc
MoazAhmedS <moazahmed0101@gmail.com>
2025-06-03 12:43:18 +0300

    settting deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base:"/Valver",
+  base:"/Valver/",
   plugins: [
     react(),
     mode === 'development' &&
